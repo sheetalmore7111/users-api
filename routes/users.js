ccd6bb2e49cf89ae4b16e3d807673321f23dd2e6
sheetalmore7111 <sheetalmore7111@gmail.com>
2023-12-28 16:24:18 +0530

    added get by id code

--- routes/users.js
+++ routes/users.js
@@ -26,6 +26,30 @@ app.get("/contacts", async (req, res) => {
     });
 });
 
+// get User by Id
+app.get("/contacts/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    // Check if userId is a valid positive integer
+    if (!/^[1-9]\d*$/.test(id)) {
+      return res.status(400).json({ error: "Invalid userId format" });
+    }
+
+    const query = "SELECT * FROM users WHERE id = ?";
+    const [user] = await connection.query(query, [id]);
+
+    if (user.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // Post Users:
 app.post("/createContact", async (req, res) => {
   const {
